e1aab9e91f8324f2b3a1697670d5bd44e838f8d6
Marcelo Forets <mforets@gmail.com>
2022-08-16 17:54:51 -0300

    updates

--- clases/2022_08_16/2022_08_16.jl
+++ clases/2022_08_16/2022_08_16.jl
@@ -11,8 +11,35 @@
 
 # explorar vs explotar (utilizar)
 
-# ----------------------------------------  y = D
-#
-#   \
-#    \
-# ----------------------------------------  y = 0
+# Idea : resolver un caso sencillo
+
+using Plots
+using StatsPlots
+
+rand()
+histogram(rand(10_000))
+
+randn()
+histogram(randn(10_000))
+
+using Distributions
+
+N = Normal(0.0, 1.0)
+histogram!(rand(N, 10_000), color = :orange)
+
+N = Normal(1.0, 5.0)
+histogram!(rand(N, 10_000), color = :orange)
+
+##
+# Entornos
+##
+
+# Project.toml
+# Manifest.toml
+
+# Archivos de proyecto ("project files")
+
+# Hay un proyecto por defecto que se genera cuando instalo paquetes en Julia.
+# Es lo que utiliza el "entorno" por defecto.
+
+# Si abro `julia`
--- docs/src/Herramientas/Computacion_simbolica.md
+++ docs/src/Herramientas/Computacion_simbolica.md
@@ -9,6 +9,14 @@
 
 ---
 
+
 ## Ejercicios
 
-#### 4.1. 
+!!! warning "Formato de entrega"
+    El formato de entrega es análogo al utilizado en los entregables anteriores, ver [Ejercicio 1.2 Creación de un repositorio](https://mforets.github.io/computacion-cientifica-en-julia/dev/Herramientas/Entorno_de_desarrollo/#.2.-Creaci%C3%B3n-de-un-repositorio). En particular, todos los ejercicios entregados deben ser parte de un único módulo llamado `Entregable_4` que define la constante CI asi como también exporta las funciones que se piden en cada ejercicio entregado.
+
+#### 4.1. Múltiplo de la identidad
+
+#### 4.2. 
+
+#### 4.3.
