39b256a18c8f78c6b02e3a194b2ee0580e46896b
Marcelo Forets <mforets@gmail.com>
2022-08-16 18:46:18 -0300

    agregar clase

--- clases/2022_08_16/2022_08_16.jl
+++ clases/2022_08_16/2022_08_16.jl
@@ -25,10 +25,37 @@ histogram(randn(10_000))
 using Distributions
 
 N = Normal(0.0, 1.0)
-histogram!(rand(N, 10_000), color = :orange)
+histogram(rand(N, 10_000), color = :orange)
 
-N = Normal(1.0, 5.0)
-histogram!(rand(N, 10_000), color = :orange)
+N = Normal(10.0, 5.0)
+histogram!(rand(N, 10_000), color = :red)
+
+struct Palanca
+    distribucion::Normal
+end
+
+struct Maquina
+    palancas::Vector{Palanca}
+end
+
+function Maquina(; k::Int = 10, μ = zeros(k), σ = ones(k))
+    vec = [Palanca(Normal(μi, σi)) for (μi, σi) in zip(μ, σ)]
+    Maquina(vec)
+end
+
+function jugar(m::Maquina, i::Int)
+    p = m.palancas[i]
+    rand(p.distribucion)
+end
+
+
+m = Maquina(k = 2, μ = [1.0, 5.0])
+histogram([jugar(m, 1) for _ in 1:1000], color = :blue)
+histogram!([jugar(m, 2) for _ in 1:1000], color = :orange)
+
+# Algoritmo 1:
+N = 1000
+proxima = jugar(m, i)
 
 ##
 # Entornos
@@ -43,3 +70,11 @@ histogram!(rand(N, 10_000), color = :orange)
 # Es lo que utiliza el "entorno" por defecto.
 
 # Si abro `julia`
+
+# major.minor.patch
+
+# packages
+
+# ----------------------
+
+
