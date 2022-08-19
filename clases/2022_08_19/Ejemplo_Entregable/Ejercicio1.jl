7c7a95bb04ee95b50a5c4b6e4cf8cd30d6547e1e
Marcelo Forets <mforets@gmail.com>
2022-08-19 19:20:02 -0300

    agregar clase

---
+++ clases/2022_08_19/Ejemplo_Entregable/Ejercicio1.jl
@@ -0,0 +1,9 @@
+export mi_funcion
+
+struct A
+    x::Float64
+end
+
+function mi_funcion(x)
+    2sin(x)
+end
---
+++ clases/2022_08_19/Ejemplo_Entregable/Entregable.jl
@@ -0,0 +1,6 @@
+module Entregable
+const CI = "43401869"
+
+include("Ejercicio1.jl")
+include("Ejercicio2.jl")
+end
