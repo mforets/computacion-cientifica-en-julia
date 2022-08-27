6367dc6439426900bf47525978597dacb1100771
Marcelo Forets <mforets@gmail.com>
2022-08-26 22:35:09 -0300

    updates

---
+++ clases/2022_08_26/2022_08_26.jl
@@ -0,0 +1,39 @@
+# Pluto.jl notebooks
+
+] add Pluto
+using Pluto
+Pluto.run()
+
+# Tipos abstractos y tipos compuestos
+
+#=
+julia> AbstractTrees.children(t::Type) = subtypes(t)
+
+julia> print_tree(Number)
+Number
+├─ Complex
+└─ Real
+   ├─ AbstractFloat
+   │  ├─ BigFloat
+   │  ├─ Float16
+   │  ├─ Float32
+   │  └─ Float64
+   ├─ AbstractIrrational
+   │  └─ Irrational
+   ├─ Integer
+   │  ├─ Bool
+   │  ├─ Signed
+   │  │  ├─ BigInt
+   │  │  ├─ Int128
+   │  │  ├─ Int16
+   │  │  ├─ Int32
+   │  │  ├─ Int64
+   │  │  └─ Int8
+   │  └─ Unsigned
+   │     ├─ UInt128
+   │     ├─ UInt16
+   │     ├─ UInt32
+   │     ├─ UInt64
+   │     └─ UInt8
+   └─ Rational
+   =#
