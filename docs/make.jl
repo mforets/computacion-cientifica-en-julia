7a5c322c17bec3814d96d10effa7d889721d71fe
Marcelo Forets <mforets@gmail.com>
2023-03-18 15:26:15 +0000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -19,6 +19,7 @@ makedocs(
     pages = [
         "Generalidades" => "index.md",
         "Proyectos" => "proyectos.md",
+        "Entregables" => Any["Entregable 1" => "Entregable1.md"],
         "Herramientas básicas" => Any[
             "Alcance del curso" => "Herramientas/Alcance_del_curso.md",
             "Entorno de desarrollo" => "Herramientas/Entorno_de_desarrollo.md",
