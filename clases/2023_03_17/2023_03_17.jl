bf65bedf1a28c4ce563471e847b2901e623f7349
Marcelo Forets <mforets@gmail.com>
2023-03-18 15:29:48 +0000

    Create 2023_03_17.jl

---
+++ clases/2023_03_17/2023_03_17.jl
@@ -0,0 +1,33 @@
+## VSCode
+
+# SELECCIONAR Y APRETAR SHIFT + ENTER
+doble(x) = 2x
+
+doble(1)
+
+# Cadenas de caracteres
+x = "Hola"
+
+typeof(x)
+
+replace(x, "H" => "A")
+
+reverse(x)
+
+x[1]
+x[4]
+
+x[begin]
+x[end]
+
+# Diccionarios.
+# key - value
+dict = Dict("Hello" => "Hola", "Bye" => "Chau")
+
+dict["Hello"]
+
+alturas = Dict("Marcelo" => 1.74)
+
+alturas["Roberto"] = 1.8
+
+alturas
