cfdcb9b8439f228787619e429e7648c3c43a64ec
Marcelo Forets <marcelo@planting.space>
2023-03-30 20:25:18 -0300

    ideas ej 1

--- clases/2023_03_30/ejercicio1-ideas.jl
+++ clases/2023_03_30/ejercicio1-ideas.jl
@@ -1,9 +1,20 @@
-x = [1, 2, 3, 4, 5, 6]
+# Sea x un vector del largo deseado.
+# Para cada i de 1 al largo de x
+#   x[i] = obtener_valor()
+# Devolver matriz de tamano deseado.
+#
+x = 1:100
 
-reshape(x, (3, 2))
+# a <- numero aleatorio
+# mientras que (a < 1) || (a > 10)
+#   a <- numero aleatorio
+# devolver a
+
+
+reshape(x, (10, 10))
 
 [
-    x[1] x[2]
-    x[3] x[4]
-    x[5] x[6]
+    x[1] x[4]
+    x[2] x[5]
+    x[3] x[6]
 ]
